d13ec8eab2a9a182b62b1cea982a72be472d3799
BraianMendes <00.braian.dev@gmail.com>
2020-04-08 12:24:30 -0300

    Frontend base done and styled with styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Tetris from './components/Tetris';
 
-const App = () => {
-  <div className="App">
-    <Tetris />
-  </div>
+class App extends React.Component {
+  render(){
+    return (
+        <div className="App">
+          <Tetris />
+        </div>
+      )
+  }
 };
 
 export default App;
--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,1 +1,10 @@
 // StartButton.js
+
+import React from 'react';
+import { StyledStartButton } from './styles/StyledStartButton';
+
+const StartButton = ({ callback }) => (
+	<StyledStartButton onClick={callback}>Start Game</StyledStartButton>
+)
+
+export default StartButton;
